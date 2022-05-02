6a3f5548953eacce695d8161b8b5be0b953e861a
Davide Giacomini <giacomini.davide@outlook.com>
2022-05-01 20:15:59 -0500

    Converted ticks and velocity in angular velocities

--- src/omnidirectional-robot-odometry/include/omnidirectional-robot-odometry/sub.h
+++ src/omnidirectional-robot-odometry/include/omnidirectional-robot-odometry/sub.h
@@ -1,13 +1,25 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "geometry_msgs/PoseStamped.h"
+#include "sensor_msgs/JointState.h"
 
-class Subscriber {
+enum class Wheel {
+    FL = 0,
+    FR = 1,
+    RL = 2,
+    RR = 3
+};
+
+class KinematicsToOdometry {
 public:
-    Subscriber();
+    KinematicsToOdometry();
     void main_loop();
-    void chatterCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+    void compute_velocities(const sensor_msgs::JointState::ConstPtr& msg);
 private:
-    ros::NodeHandle n;
+    static const int N = 42;// Encoders resolution
+    static const int T = 5; // 5:1 is the gear ratio
+
+    ros::NodeHandle nh;
     ros::Subscriber sub;
+    double ticks_wheels[4][2];
+    double stamp_ns[2] = { -1, -1 };
 };
--- src/omnidirectional-robot-odometry/src/sub.cpp
+++ src/omnidirectional-robot-odometry/src/sub.cpp
@@ -1,15 +1,49 @@
 #include <omnidirectional-robot-odometry/sub.h>
 
-Subscriber::Subscriber()
+KinematicsToOdometry::KinematicsToOdometry()
 {
-  this->sub = this->n.subscribe("robot/pose", 1000, &Subscriber::chatterCallback, this);
+  this->sub = this->nh.subscribe("wheel_states", 1000, &KinematicsToOdometry::compute_velocities, this);
 }
 
-void Subscriber::chatterCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-  ROS_INFO("I heard: [%lf]", msg->pose.position.x);
+void KinematicsToOdometry::compute_velocities(const sensor_msgs::JointState::ConstPtr& msg) {
+
+  this->stamp_ns[0] = this->stamp_ns[1];
+  this->stamp_ns[1] = msg->header.stamp.nsec;
+
+  if (this->stamp_ns[0] == -1) {
+    return;
+  }
+
+  for (int wheel = 0; wheel < 4; wheel++) {
+    this->ticks_wheels[wheel][0] = this->ticks_wheels[wheel][1];
+    this->ticks_wheels[wheel][1] = msg->position[wheel];
+  }
+
+
+  double ang_vel_wheels1[4];
+  double ang_vel_wheels2[4];
+
+  for (int wheel = 0; wheel < 4; wheel++) {
+    ang_vel_wheels1[wheel] = (this->ticks_wheels[wheel][1]-this->ticks_wheels[wheel][0])/(this->stamp_ns[1]-this->stamp_ns[0])/this->N/this->T*2*M_PI*pow(10, 9);
+  }
+
+  for (int wheel = 0; wheel < 4; wheel++) {
+    ang_vel_wheels2[wheel] = msg->velocity[wheel]/60/this->T;
+  }
+
+  std::cout << ang_vel_wheels1[0] << std::endl;
+  std::cout << ang_vel_wheels2[0] << std::endl;
+  std::cout << ang_vel_wheels1[1] << std::endl;
+  std::cout << ang_vel_wheels2[1] << std::endl;
+  std::cout << ang_vel_wheels1[2] << std::endl;
+  std::cout << ang_vel_wheels2[2] << std::endl;
+  std::cout << ang_vel_wheels1[3] << std::endl;
+  std::cout << ang_vel_wheels2[3] << std::endl;
+  std::cout << *msg << std::endl;
+  // ROS_INFO("I heard: [%lf]", msg->pose.position.x);
 }
 
-void Subscriber::main_loop()
+void KinematicsToOdometry::main_loop()
 {
   ros::spin();
 }
@@ -17,7 +51,7 @@ void Subscriber::main_loop()
 int main(int argc, char **argv) {
   ros::init(argc, argv, "listener");
 
-  Subscriber my_subscriber;
+  KinematicsToOdometry my_subscriber;
   my_subscriber.main_loop();
 
   return 0;
