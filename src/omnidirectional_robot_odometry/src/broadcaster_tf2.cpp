b6670d106cff3d6958ba8ce69d498e4497857451
Davide Giacomini <giacomini.davide@outlook.com>
2022-05-04 22:45:00 -0500

    Modified launch file and copied broadcaster from slides to understand whats happening

---
+++ src/omnidirectional_robot_odometry/src/broadcaster_tf2.cpp
@@ -0,0 +1,47 @@
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+
+class TfBroad
+{
+public:
+  TfBroad() {
+    sub = n.subscribe("/turtle1/pose", 1000, &TfBroad::callback, this);
+  }
+
+  void callback(const turtlesim::Pose::ConstPtr& msg){
+    // set header
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "world";
+    transformStamped.child_frame_id = "turtle";
+    // set x,y
+    transformStamped.transform.translation.x = msg->x;
+    transformStamped.transform.translation.y = msg->y;
+    transformStamped.transform.translation.z = 0.0;
+    // set theta
+    tf2::Quaternion q;
+    q.setRPY(0, 0, msg->theta);
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+    // send transform
+    br.sendTransform(transformStamped);
+  }
+
+private:
+  ros::NodeHandle n; 
+  tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+  ros::Subscriber sub;
+};
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "tf_broadcast");
+  TfBroad my_tf_broadcaster;
+  ros::spin();
+  return 0;
+}
